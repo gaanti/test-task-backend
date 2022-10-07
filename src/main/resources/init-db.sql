cae1b393a383bc7274de8f84a78c347b17ecb49a
gaanti <gaskevich06@gmail.com>
2022-10-07 13:48:32 +0300

    deploy to heroku

--- src/main/resources/init-db.sql
+++ src/main/resources/init-db.sql
@@ -1,10 +1,10 @@
 DROP DATABASE IF EXISTS test_task_db;
-DROP USER IF EXISTS `test-task-admin`@`%`;
-DROP USER IF EXISTS `test-task-user`@`%`;
+DROP USER IF EXISTS `test_task_admin`@`%`;
+DROP USER IF EXISTS `test_task_user`@`%`;
 CREATE DATABASE IF NOT EXISTS test_task_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS `test-task-admin`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
+CREATE USER IF NOT EXISTS `test_task_admin`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
-CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `test_task_db`.* TO `test-task-admin`@`%`;
-CREATE USER IF NOT EXISTS `test-task-user`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `test_task_db`.* TO `test-task-user`@`%`;
+CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `test_task_db`.* TO `test_task_admin`@`%`;
+CREATE USER IF NOT EXISTS `test_task_user`@`%` IDENTIFIED WITH mysql_native_password BY 'password';
+GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `test_task_db`.* TO `test_task_user`@`%`;
 FLUSH PRIVILEGES;
